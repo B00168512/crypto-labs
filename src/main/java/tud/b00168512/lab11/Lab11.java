e91367ab23ae685a6c772058a85e76e60a4f6002
Grygorii Kosik <b00168512@tudublin.ie>
2025-03-22 17:15:51 +0000

    Lab11 implementation and tests

---
+++ src/main/java/tud/b00168512/lab11/Lab11.java
@@ -0,0 +1,152 @@
+package tud.b00168512.lab11;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Decrypts ciphertext by simply re-applying steps 1, 2 and 3 in reverse order.
+ * Decrypting 'intercepted.txt' file results in plaintext message: "Why do elephants have big ears?".
+ * It was calculated that the encryption was done in 67 rounds.
+ */
+public class Lab11 {
+
+    private static final String ASCII_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+
+    private static final String TRANSLATE_FROM = "zyxwvutsrqponZYXWVUTSRQPONmlkjihgfedcbaMLKJIHGFEDCBA";
+    private static final String TRANSLATE_TO = "mlkjihgfedcbaMLKJIHGFEDCBAzyxwvutsrqponZYXWVUTSRQPON";
+
+    private static final Map<Character, Character> TRANSLATION_TABLE;
+    private static final Map<Character, Character> REVERSE_TRANSLATION_TABLE;
+
+    private static final int DEFAULT_NUM_OF_SHIFTS = 4;
+
+    static {
+        TRANSLATION_TABLE = buildTranslationTable(TRANSLATE_FROM, TRANSLATE_TO);
+        REVERSE_TRANSLATION_TABLE = buildTranslationTable(TRANSLATE_TO, TRANSLATE_FROM);
+    }
+
+    static String makeSecret(String plaintext, int count) {
+        var base64Encoded = Base64.getEncoder().encode(plaintext.getBytes());
+        var secret = String.format("2%s", new String(base64Encoded, StandardCharsets.US_ASCII));
+        var random = new Random();
+        var randomEncoders = ENCODERS.values();
+        var encodersSize = randomEncoders.length;
+        for (int i = 0; i < count; i++) {
+            var encoder = randomEncoders[random.nextInt(encodersSize)];
+            var encoded = encoder.function.apply(secret);
+            secret = String.format("%s%s", encoder.ordinal() + 1, encoded);
+        }
+        return secret;
+    }
+
+    static Result decryptSecret(String ciphertext) {
+        return decryptSecret(ciphertext, -1);
+    }
+
+    private static Result decryptSecret(String ciphertext, int backTrackRounds) {
+        var firstChar = ciphertext.charAt(0);
+        if (firstChar > 48 && firstChar < 52) { // 1, 2, 3
+            var stepId = Character.digit(firstChar, 10);
+            stepId = stepId - 1;
+            var decoder = DECODERS.values()[stepId];
+            var decoded = decoder.function.apply(ciphertext.substring(1));
+            return decryptSecret(decoded, backTrackRounds + 1);
+        }
+        return new Result(backTrackRounds, ciphertext);
+    }
+
+    static String step1(String secret) {
+        return translate(secret, TRANSLATION_TABLE);
+    }
+
+    static String step2(String input) {
+        return new String(Base64.getEncoder().encode(input.getBytes()), StandardCharsets.US_ASCII);
+    }
+
+    static String step3(String input) {
+        var lowercaseAlpha = ASCII_LOWERCASE;
+        var shifted = shiftString(lowercaseAlpha, DEFAULT_NUM_OF_SHIFTS);
+        var translationTable = buildTranslationTable(lowercaseAlpha, shifted);
+        return translate(input, translationTable);
+    }
+
+    private static Map<Character, Character> buildTranslationTable(String translateFrom, String translateTo) {
+        Map<Character, Character> table = new LinkedHashMap<>(translateFrom.length());
+        for (int i = 0; i < translateFrom.length(); i++) {
+            table.put(translateFrom.charAt(i), translateTo.charAt(i));
+        }
+        return table;
+    }
+
+    private static String translate(String secret, Map<Character, Character> translationTable) {
+        var translation = new char[secret.length()];
+        for (int i = 0; i < secret.length(); i++) {
+            var secretChar = secret.charAt(i);
+            translation[i] = translationTable.getOrDefault(secretChar, secretChar);
+        }
+        return new String(translation);
+    }
+
+    private static String shiftString(String input, int shift) {
+        int len = input.length();
+        shift = shift % len; // ensure a shift value is within alphabet (26 chars)
+        return IntStream.concat(IntStream.range(shift, len), IntStream.range(0, shift))
+                .mapToObj(i -> String.valueOf(input.charAt(i)))
+                .collect(Collectors.joining());
+    }
+
+    static String decodeStep1(String input) {
+        return translate(input, REVERSE_TRANSLATION_TABLE);
+    }
+
+    static String decodeStep2(String input) {
+        return new String(Base64.getDecoder().decode(input.getBytes()), StandardCharsets.US_ASCII);
+    }
+
+    static String decodeStep3(String input) {
+        var lowercaseAlpha = ASCII_LOWERCASE;
+        var shifted = shiftString(lowercaseAlpha, DEFAULT_NUM_OF_SHIFTS);
+        var reversedTranslationTable = buildTranslationTable(shifted, lowercaseAlpha);
+        return translate(input, reversedTranslationTable);
+    }
+
+    enum ENCODERS {
+
+        STEP_1(Lab11::step1),
+
+        STEP_2(Lab11::step2),
+
+        STEP_3(Lab11::step3);
+
+        final Function<String, String> function;
+
+        ENCODERS(Function<String, String> function) {
+            this.function = function;
+        }
+    }
+
+    enum DECODERS {
+
+        STEP_1(Lab11::decodeStep1),
+
+        STEP_2(Lab11::decodeStep2),
+
+        STEP_3(Lab11::decodeStep3);
+
+        final Function<String, String> function;
+
+        DECODERS(Function<String, String> function) {
+            this.function = function;
+        }
+    }
+
+    record Result(int rounds, String plaintext) {
+
+    }
+}
---
+++ src/test/java/tud/b00168512/lab11/Lab11Test.java
@@ -0,0 +1,77 @@
+package tud.b00168512.lab11;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lab11Test {
+
+    @Test
+    void decodeStep1() {
+        var secret = "hello1";
+        var ciphertext = Lab11.ENCODERS.STEP_1.function.apply(secret);
+        var plaintext = Lab11.DECODERS.STEP_1.function.apply(ciphertext);
+        assertEquals(secret, plaintext);
+    }
+
+    @Test
+    void decodeStep2() {
+        var secret = "hello2";
+        var ciphertext = Lab11.ENCODERS.STEP_2.function.apply(secret);
+        var plaintext = Lab11.DECODERS.STEP_2.function.apply(ciphertext);
+        assertEquals(secret, plaintext);
+    }
+
+    @Test
+    void decodeStep3() {
+        var secret = "hello3";
+        var ciphertext = Lab11.ENCODERS.STEP_3.function.apply(secret);
+        var plaintext = Lab11.DECODERS.STEP_3.function.apply(ciphertext);
+        assertEquals(secret, plaintext);
+    }
+
+    @Test
+    void decodeSecret1() {
+        var secret = "hello secret";
+        var rounds = 4;
+        var ciphertext = Lab11.makeSecret(secret, rounds);
+        var result = Lab11.decryptSecret(ciphertext);
+        assertEquals(rounds, result.rounds());
+        assertEquals(secret, result.plaintext());
+    }
+
+    @Test
+    void decodeSecret2() {
+        var secret = "Lab 11- Encrypted Evidence File (Challenge Lab)";
+        var rounds = 10;
+        var ciphertext = Lab11.makeSecret(secret, rounds);
+        var result = Lab11.decryptSecret(ciphertext);
+        assertEquals(rounds, result.rounds());
+        assertEquals(secret, result.plaintext());
+    }
+
+    @Test
+    void decodeInterceptedMessage() {
+        var secret = getInterceptedCiphertext();
+        var result = Lab11.decryptSecret(secret);
+        assertEquals("Why do elephants have big ears?", result.plaintext());
+        assertEquals(67, result.rounds());
+    }
+
+    private static String getInterceptedCiphertext() {
+        try {
+            var resource = "/lab11/intercepted.txt";
+            var url = Lab11.class.getResource(resource);
+            if (url == null) {
+                throw new IllegalArgumentException("Resource does not exist: " + resource);
+            }
+            return Files.readString(Paths.get(url.toURI()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
